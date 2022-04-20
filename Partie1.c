33c5f37ea081187e5208df230800b3d7ab221232
EvaGia <103753133+EvaGia@users.noreply.github.com>
2022-04-20 17:40:15 +0200

    Update Partie1.c
    - j'ai juste commenté, changé l'affichage dans is_primitive_naive, 
    - revoir modpow(?)
    - que fait extended_gcd ?

--- Partie1.c
+++ Partie1.c
@@ -1,14 +1,18 @@
 #include "Partie1.h"
 
+/* renvoie 1 si p (impair) est premier, 0 sinon */
 int is_primitive_naive(long int p){
 	for(int i = 3 ; i < p - 1 ; i++){
 		if(p % i == 0){
-		    printf("Le nombre est premier \n");
-			return 0; 
+		    printf("Le nombre %ld n'est pas premier \n", p);
+		    return 0; 
 		}
-	}return 1; 
+	}
+	printf("Le nombre %ld est premier \n", p)
+	return 1; 
 }
 
+/* retourne a (puissance m) mod n */
 long modpow_naive(long a, long m, long n){
 	long res ;
 	long vcm = 1; 
@@ -21,6 +25,7 @@ long modpow_naive(long a, long m, long n){
 	return res;
 }
 
+/* retourne a (puissance m) mod n */
 int modpow(long a, long m, long n){ 
 	if (m == 0)
 		return 1; 
@@ -28,6 +33,7 @@ int modpow(long a, long m, long n){
 		return 0; 
 	
 	long newa;
+	/* si m est pair */
 	if(m % 2 == 0 ){
 		newa = modpow(a, m / 2, n) % n;
 		newa = (newa * newa) % n;
@@ -40,6 +46,7 @@ int modpow(long a, long m, long n){
 	return (int)((newa + n )% n);
 }
 
+/* renvoie 1 si a est un temoin de miller pour p, 0 sinon */
 int witness ( long a , long b , long d , long p ) {
 	long x = modpow (a ,d , p ) ;
 	if ( x == 1) {
@@ -54,10 +61,12 @@ int witness ( long a , long b , long d , long p ) {
 	return 1;
 }
 
+/* renvoie un entier long aleatoire entre low et up inclus */
 long rand_long ( long low , long up ) {
 	return rand () % ( up - low +1) + low ;
 }
 
+/* renvoie 1 si p est premier, 0 sinon */
 int is_prime_miller ( long p , int k ) {
 	if ( p == 2) {
 		return 1;
@@ -84,10 +93,14 @@ int is_prime_miller ( long p , int k ) {
 	return 1;
 }
 
+/* retourne un nombre premier de taille comprise entre low_size et up_size */:
 long random_prime_number(int lowSize, int upSize, int k){
 	long max = pow(2, lowSize);
 	long min = pow(2,upSize);
 	long p ; 
+	
+	//on genere un nouveau p tant qu'il ne passe pas le test de Miller-Rabin
+	
 	do{
 		p = rand_long(min, max);
 	} while (is_prime_miller(p, k) == 0);
@@ -108,12 +121,14 @@ long extended_gcd ( long s , long t , long *u , long * v ) {
 	return gcd ;
 }
 
+/* genere les valeurs pour la cle publique pkey (s, n) et la cle secrete skey (u, n) */
 void generate_key_values(long p, long q, long *n, long *s,long *u){
 	*n = p * q;
 	long t = (p - 1) * (q - 1);
 	long v;
 	long pgcd = extended_gcd(*s,t,u,&v);
-	 
+	
+	/* on genere aleatoirement des entiers s inferieurs a t jusqu'a ce que PGCD(s,t) = 1*/
 	while (pgcd != 1){
 		*s = rand()% t;  
 		pgcd = extended_gcd(*s,t,u,&v);
@@ -123,11 +138,12 @@ void generate_key_values(long p, long q, long *n, long *s,long *u){
 	}
 }
 
+/* chiffre la chaine avec la cle publique (s,n), retourne un tableau de long */
 long *encrypt(char* chaine, long s, long n){
 	int lc = strlen(chaine);
-    long * res = (long *)malloc(sizeof(long)*lc);
+    	long * res = (long *)malloc(sizeof(long)*lc);
 	if (res == NULL){
-		printf("Erreur malloc encrypt");
+		printf("Erreur malloc encrypt\n");
 	}
 
     int i = 0;
@@ -138,6 +154,7 @@ long *encrypt(char* chaine, long s, long n){
     return res;
 }
 
+/* retourne le message a l'origine du tableau de long decrypte avec la cle secrete (u,n) */
 char* decrypt(long* crypted, int size, long u, long n ){
 	char* dechiffre = (char*)malloc(sizeof(char)*(size+1));
 	if(dechiffre == NULL){
