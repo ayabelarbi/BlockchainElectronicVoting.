468fc4aa7352c55ce34566826517c95c80c07748
EvaGia <103753133+EvaGia@users.noreply.github.com>
2022-04-22 23:04:57 +0200

    Update mainP4.c

--- mainP4.c
+++ mainP4.c
@@ -3,12 +3,28 @@
 
 int main(){
 
+//test SHA256
+
 const char * s = "Rosetta code ";
 unsigned char * d = SHA256(s , strlen( s ), 0) ;
 int i ;
+
 for (i = 0; i<SHA256_DIGEST_LENGTH ; i++)
     printf("%02x" , d[i]) ;
 putchar('\n') ;
+
+/*test lecture ecriture
+Block * b = ...
+ecriture_bloc(b);
+Block * 2 = lecture_bloc("bloc.txt"); */
+
+/*test bloc to str
+printf("Bloc : \n%s\n", block_to_str(b));  */
+
+//test str_hache
+char * s2 = "Rosetta code";
+char * hash = str_hache(s2);
+printf("%s\n", hash);
 
     return 0;
 }
