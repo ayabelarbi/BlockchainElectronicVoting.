ad74be6952ccdfa4859a8f02094c0e637a571926
EvaGia <103753133+EvaGia@users.noreply.github.com>
2022-04-23 20:50:07 +0200

    Update mainP4.c

--- mainP4.c
+++ mainP4.c
@@ -3,28 +3,61 @@
 
 int main(){
 
-//test SHA256
+ //test SHA256
+    printf("TEST SHA256\n\n");
 
-const char * s = "Rosetta code ";
-unsigned char * d = SHA256(s , strlen( s ), 0) ;
-int i ;
+    const char * s = "Rosetta code ";
+    unsigned char * d = SHA256(s , strlen( s ), 0) ;
+    int i ;
 
-for (i = 0; i<SHA256_DIGEST_LENGTH ; i++)
-    printf("%02x" , d[i]) ;
-putchar('\n') ;
+    printf("hash de rosetta code :\n");
+    for (i = 0; i<SHA256_DIGEST_LENGTH ; i++)
+        printf("%02x" , d[i]) ;
+    putchar('\n') ;
+    printf("\n\n");
 
-/*test lecture ecriture
-Block * b = ...
-ecriture_bloc(b);
-Block * 2 = lecture_bloc("bloc.txt"); */
 
-/*test bloc to str
-printf("Bloc : \n%s\n", block_to_str(b));  */
+    //CREATION D'UN BLOC
+    printf("CREATION D'UN BLOC\n");
+    Block * b = (Block *)malloc(sizeof(Block));
+    Key* pkey = (Key*)malloc(sizeof(Key));
+    Key* skey = (Key*) malloc(sizeof(Key));
+    init_pair_keys(pkey,skey,3,7); 
+    b->author = pkey;
+    CellProtected * votes = read_protected("declaration.txt");
+    b->votes = votes;
+    unsigned char * hash = (unsigned char *)malloc(sizeof(unsigned char)*256);
+    unsigned char * previous_hash = (unsigned char *)malloc(sizeof(unsigned char)*256);
+    b->hash = hash;
+    b->previous_hash = previous_hash;
+    b->nonce = 0;
 
-//test str_hache
-char * s2 = "Rosetta code";
-char * hash = str_hache(s2);
-printf("%s\n", hash);
+    /*
 
+    
+    //TEST LECTURE ECRITURE + bloc_to_str
+    printf("TEST LECTURE ECRITURE + bloc_to_str\n\n");
+
+    ecriture_bloc(b);
+    Block * b2 = lecture_bloc("bloc.txt");
+
+    printf("bloc b :\n%s\n", block_to_str(b));
+    printf("Bloc b2 :\n%s\n", block_to_str(b2));
+
+    
+    //TEST str_to_hash
+    printf("TEST str_to_hash\n\n");
+
+    char * s2 = "Rosetta code";
+    unsigned char * hash2 = str_hache(s2);
+    printf("%s\n", hash2);
+
+    //TEST compute_prof_of_work + verify
+    printf("TEST compute_proof_of_work\\n");
+
+    compute_proof_of_work(b, 17);
+
+    */
     return 0;
+
 }
