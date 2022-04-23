2f7d8e19c651dbba022d5319f1f866a63dddec94
EvaGia <103753133+EvaGia@users.noreply.github.com>
2022-04-23 20:51:15 +0200

    Update Partie4.c

--- Partie4.c
+++ Partie4.c
@@ -95,16 +95,16 @@ char* block_to_str(Block* block){
     return res;
 }
 
-char * str_hache(char * chaine){
+unsigned char * str_hache(char * st){
 
-    unsigned int int_tmp;
+    /*unsigned int int_tmp;
     unsigned char * str_tmp = (unsigned char*)malloc(sizeof(unsigned char)*4);
-    unsigned char * res = (unsigned char*)malloc(sizeof(unsigned char)*(strlen(chaine)*4));
+    unsigned char * res = (unsigned char*)malloc(sizeof(unsigned char)*(strlen(st)*4));
     strcat(res, "");
     int ind_res = 0;
     int k = 0;
-    for(int i = 0; i< strlen(chaine); i++){
-        sscanf(&chaine[i], "%02x",&int_tmp);
+    for(int i = 0; i< strlen(st); i++){
+        sscanf(&st[i], "%02x",&int_tmp);
         *str_tmp = int_tmp;
         strcat(res, str_tmp);
         printf("%s\n", res);
@@ -112,6 +112,68 @@ char * str_hache(char * chaine){
     }
     strcat(res, "\0");
     return res;
+    
+    //OU :
+
+    unsigned char * s = SHA256 (st , strlen ( st ) , 0);
+    unsigned char * res = (unsigned char *)malloc(sizeof(unsigned char)*strlen(s)*3);
+    unsigned char * res_courant = res;
+    char tmp[4];
+    int j = 0;
+    for ( int i = 0; i < SHA256_DIGEST_LENGTH ; i ++){
+        if ((i+1)%4==0){
+            sscanf(tmp, "%02x", &res_courant);
+            res_courant = res_courant+4;
+        } else {
+            tmp[i]=s[i];
+        }
+        
+    }
+    return res;
+
+    */
+
+    //VERSION DE DANAEL
+    unsigned char * res = (unsigned char *) malloc(SHA256_DIGEST_LENGTH*sizeof(unsigned char));
+    char buffer_l[4];
+    unsigned int stock;
+    int j = 0;
+    for(int i = 0 ; i < 3*SHA256_DIGEST_LENGTH ; i = i + 3){
+        buffer_l[0] = st[i];
+        buffer_l[1] = st[i+1];
+        buffer_l[2] = st[i+2];
+        buffer_l[3] = '\0';
+
+
+        if(sscanf(buffer_l,"%02x",&stock)!=1){
+            printf("Erreur de formatage du hachage hexadécimal\n");
+            free(res);
+            return NULL;
+        }
+        res[j] = stock;
+        j++;
+    }
+    return res;
+}
+
+void compute_proof_of_work(Block * B, int d){
+    B->nonce = 0;
+    int bool;
+    do {
+        bool = 1;                      //booleen vrai si la valeur hachee du bloc commence par d 0, on l'initialise a vrai
+        char * str_block = block_to_str(B);
+        B->hash = SHA256(str_block, strlen(str_block), 0);
+        unsigned char * hash = B->hash;
+        for(int i = 0; i < d*4; i++){
+            //si on trouve autre chose qu'un 0 on met le booleen a faux, on sort de la boucle et on incremente nonce
+            if (hash[i] != 0){
+                bool = 0;
+                i = d*4;
+                B->nonce = B->nonce +1;      
+            }
+        }
+    }  while (bool != 1);
+    
 }
 
 
