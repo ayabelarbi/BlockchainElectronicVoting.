44339703e9e6c8306070db4f9cea0f8c5050d28b
EvaGia <103753133+EvaGia@users.noreply.github.com>
2022-04-24 20:15:17 +0200

    Update mainP4.c

--- mainP4.c
+++ mainP4.c
@@ -3,22 +3,23 @@
 
 int main(){
 
- //test SHA256
-    printf("TEST SHA256\n\n");
+    //TEST SHA256
+    printf("TEST SHA256\n");
 
-    const char * s = "Rosetta code ";
+    const char * s = "Rosetta code";
     unsigned char * d = SHA256(s , strlen( s ), 0) ;
     int i ;
 
     printf("hash de rosetta code :\n");
-    for (i = 0; i<SHA256_DIGEST_LENGTH ; i++)
+    for (i = 0; i<SHA256_DIGEST_LENGTH ; i++){
         printf("%02x" , d[i]) ;
+    }
+        
     putchar('\n') ;
     printf("\n\n");
 
 
     //CREATION D'UN BLOC
-    printf("CREATION D'UN BLOC\n");
     Block * b = (Block *)malloc(sizeof(Block));
     Key* pkey = (Key*)malloc(sizeof(Key));
     Key* skey = (Key*) malloc(sizeof(Key));
@@ -26,38 +27,65 @@ int main(){
     b->author = pkey;
     CellProtected * votes = read_protected("declaration.txt");
     b->votes = votes;
-    unsigned char * hash = (unsigned char *)malloc(sizeof(unsigned char)*256);
+    
     unsigned char * previous_hash = (unsigned char *)malloc(sizeof(unsigned char)*256);
-    b->hash = hash;
+    //on prend un previous hash au hasrad
+    previous_hash = SHA256(d, strlen(d),0);
     b->previous_hash = previous_hash;
-    b->nonce = 0;
+    b->nonce = 0; 
 
-    /*
+    unsigned char * hash = (unsigned char *)malloc(sizeof(unsigned char)*256);
+    hash = SHA256(block_to_str(b), strlen(block_to_str(b)), 0);
+    b->hash = hash;
+    
+    //TEST HASH_TO_STR
+    printf("TEST HASH_TO_STR\n");
+    unsigned char * hash_test = (unsigned char *)malloc(sizeof(unsigned char)*SHA256_DIGEST_LENGTH);
+    for(int i = 0; i<SHA256_DIGEST_LENGTH; i++){
+        hash_test[i]=0;
+    }
+
+    printf("%s\n", hash_to_str(hash_test));
+    
+    //TEST BLOC_TO_STR
+    printf("\nTEST BLOC_TO_STR\n");
+    printf("bloc b :\n%s\n\n", block_to_str(b));
+
+
+    //TEST VERIFY_BLOCK + COMPUTE_PROOF_OF_WORK
 
     
-    //TEST LECTURE ECRITURE + bloc_to_str
-    printf("TEST LECTURE ECRITURE + bloc_to_str\n\n");
+    printf("hash du bloc avant de le rendre valide : %s\n", hash_to_str(b->hash));
+
+    if (verify_block(b,3)==1){
+        printf("le bloc est valide !\n");
+    } else {
+        printf("le bloc n'est pas valide\n");
+    }
+
+    compute_proof_of_work(b, 3);
+   
+    printf("hash du bloc apres l'avoir rendu valide : %s\n", hash_to_str(b->hash));
+
+    if (verify_block(b,3)==1){
+        printf("le bloc est valide !\n");
+    } else {
+        printf("le bloc n'est pas valide\n");
+    }
+
+
+    /*
+    //TEST LECTURE ECRITURE
+    printf("TEST LECTURE ECRITURE\n\n");
 
     ecriture_bloc(b);
     Block * b2 = lecture_bloc("bloc.txt");
-
-    printf("bloc b :\n%s\n", block_to_str(b));
-    printf("Bloc b2 :\n%s\n", block_to_str(b2));
-
     
-    //TEST str_to_hash
-    printf("TEST str_to_hash\n\n");
-
-    char * s2 = "Rosetta code";
-    unsigned char * hash2 = str_hache(s2);
-    printf("%s\n", hash2);
-
-    //TEST compute_prof_of_work + verify
-    printf("TEST compute_proof_of_work\\n");
-
-    compute_proof_of_work(b, 17);
+    printf("On ecrit le bloc precedent dans un fichier, on creer un bloc a partir de la lecture de ce fichier et on obtient :\n");
+    printf("%s\n", block_to_str(b2));
 
     */
     return 0;
-
 }
+
+
