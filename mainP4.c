fde23ef8abfd2eded7cb31ac7f46e1fc33a68e1c
EvaGia <103753133+EvaGia@users.noreply.github.com>
2022-04-25 15:39:17 +0200

    Update mainP4.c

--- mainP4.c
+++ mainP4.c
@@ -3,6 +3,7 @@
 
 int main(){
 
+
     //TEST SHA256
     printf("TEST SHA256\n");
 
@@ -18,26 +19,33 @@ int main(){
     putchar('\n') ;
     printf("\n\n");
 
+    
 
     //CREATION D'UN BLOC
     Block * b = (Block *)malloc(sizeof(Block));
     Key* pkey = (Key*)malloc(sizeof(Key));
     Key* skey = (Key*) malloc(sizeof(Key));
     init_pair_keys(pkey,skey,3,7); 
+
+    
     b->author = pkey;
     CellProtected * votes = read_protected("declaration.txt");
     b->votes = votes;
-    
-    unsigned char * previous_hash = (unsigned char *)malloc(sizeof(unsigned char)*256);
-    //on prend un previous hash au hasrad
-    previous_hash = SHA256(d, strlen(d),0);
-    b->previous_hash = previous_hash;
-    b->nonce = 0; 
 
-    unsigned char * hash = (unsigned char *)malloc(sizeof(unsigned char)*256);
-    hash = SHA256(block_to_str(b), strlen(block_to_str(b)), 0);
-    b->hash = hash;
+
+    //on prend un previous hash au hasrad
+    //unsigned char * previous_hash = (unsigned char *)malloc(sizeof(unsigned char)*SHA256_DIGEST_LENGTH);
+    //previous_hash = SHA256(d, strlen(d),0);
+    b->previous_hash = SHA256(d, strlen(d),0);
+    b->nonce = 0; 
     
+
+    //unsigned char * hash = (unsigned char *)malloc(sizeof(unsigned char)*SHA256_DIGEST_LENGTH);  
+    //hash = SHA256(block_to_str(b), strlen(block_to_str(b)), 0);
+    b->hash = SHA256(block_to_str(b), strlen(block_to_str(b)), 0); 
+    
+    
+
     //TEST HASH_TO_STR
     printf("TEST HASH_TO_STR\n");
     unsigned char * hash_test = (unsigned char *)malloc(sizeof(unsigned char)*SHA256_DIGEST_LENGTH);
@@ -47,14 +55,14 @@ int main(){
 
     printf("%s\n", hash_to_str(hash_test));
     
+  
     //TEST BLOC_TO_STR
     printf("\nTEST BLOC_TO_STR\n");
     printf("bloc b :\n%s\n\n", block_to_str(b));
-
+ 
 
     //TEST VERIFY_BLOCK + COMPUTE_PROOF_OF_WORK
-
-    
+   
     printf("hash du bloc avant de le rendre valide : %s\n", hash_to_str(b->hash));
 
     if (verify_block(b,3)==1){
@@ -64,6 +72,7 @@ int main(){
     }
 
     compute_proof_of_work(b, 3);
+
    
     printf("hash du bloc apres l'avoir rendu valide : %s\n", hash_to_str(b->hash));
 
@@ -74,18 +83,25 @@ int main(){
     }
 
 
-    /*
+
     //TEST LECTURE ECRITURE
     printf("TEST LECTURE ECRITURE\n\n");
 
     ecriture_bloc(b);
+
+    /*
     Block * b2 = lecture_bloc("bloc.txt");
-    
     printf("On ecrit le bloc precedent dans un fichier, on creer un bloc a partir de la lecture de ce fichier et on obtient :\n");
     printf("%s\n", block_to_str(b2));
 
     */
+
+    delete_block(b);
     return 0;
+
+    
 }
+
+
 
 
