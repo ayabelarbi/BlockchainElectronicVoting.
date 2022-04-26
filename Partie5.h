ee0df68192334f5bd889d6e32c35159f908fdb35
EvaGia <103753133+EvaGia@users.noreply.github.com>
2022-04-26 18:00:30 +0200

    Update Partie5.h

--- Partie5.h
+++ Partie5.h
@@ -8,5 +8,7 @@
 void submit_vote(Protected* p); 
 void create_block(CellTree* tree, Key* author, int d);
 void add_block(int d, char* name);
+CellTree* read tree();
+Key* compute winner BT(CellTree* tree, CellKey* candidates, CellKey* voters, int sizeC, int sizeV);
 
 #endif
