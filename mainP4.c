23feda5fdb4e70c4ff5268079d814f529978ea27
cha_am <75953386+ayabelarbi@users.noreply.github.com>
2022-04-26 22:34:24 +0545

    Update mainP4.c

--- mainP4.c
+++ mainP4.c
@@ -45,6 +45,42 @@ int main(){
     b->hash = SHA256(block_to_str(b), strlen(block_to_str(b)), 0); 
     
     
+    //Bloc n°2
+    Block *b2= (Block *)malloc(sizeof(Block));
+    Key* keyb2 = (Key*)malloc(sizeof(Key));
+    Key* skeyb2 = (Key*)malloc(sizeof(Key));
+    init_pair_keys(keyb2,skeyb2,3,7);
+    b2->author = keyb2;
+    CellProtected * cellb2 = read_protected("declaration.txt");
+    b2->votes = cellb2;
+    b2->previous_hash = b->hash;
+    b2->nonce = 0;  
+    b2->hash = SHA256(block_to_str(b2), strlen(block_to_str(b2)), 0); 
+    
+    //Bloc n°3
+    Block *b3= (Block *)malloc(sizeof(Block));
+    Key* keyb3 = (Key*)malloc(sizeof(Key));
+    Key* skeyb3 = (Key*)malloc(sizeof(Key));
+    init_pair_keys(keyb3,skeyb3,3,7); 
+    b3->author = keyb3; 
+    CellProtected *cellb3 = read_protected("declaration.txt");
+    b3->votes = cellb3;
+    b3->previous_hash = b2->hash;
+    b3->hash = SHA256(block_to_str(b3), strlen(block_to_str(b3)), 0);
+    b3->nonce = 0; 
+
+
+    Block *b4= (Block *)malloc(sizeof(Block));
+    Key* keyb4 = (Key*)malloc(sizeof(Key));
+    Key* skeyb4 = (Key*)malloc(sizeof(Key));
+    init_pair_keys(keyb4,skeyb4,3,7); 
+    b4->author = keyb4; 
+    CellProtected *cellb4 = read_protected("declaration.txt");
+    b4->votes = cellb4;
+    b4->previous_hash = b3->hash;
+    b4->hash = SHA256(block_to_str(b4), strlen(block_to_str(b4)), 0);
+    b4->nonce = 0; 
+
 
     //TEST HASH_TO_STR + STR_TO_HASH
     printf("TEST HASH_TO_STR\n");
@@ -105,7 +141,28 @@ int main(){
 
     delete_block(b);
     return 0;
+printf("\n---Exercice 8 : Manipulation d'un arbre de blocs---\n");
+    //Test des fonction de gestion de l'arbre 
+    CellTree* newTree = create_node(b); 
+    CellTree* child = create_node(b2);
+    CellTree* nextchild = create_node(b3);
+    add_child(newTree, child);
+    add_child(child, nextchild);
 
+    printf("Voici le premier tree du bloc B \n");
+    print_tree(newTree);
+    
+    printf("\n");
+
+
+//LIBERATION DE MEMOIRE 
+delete_cell_protected(votes);
+delete_cell_protected(cellb2);
+delete_cell_protected(cellb3);
+delete_cell_protected(cellb4); 
+
+return 0; 
+}
     
 }
 
